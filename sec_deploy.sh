26d0f129c49629145be349aba7e3357235e5533a
momotimi <michaelmomotimi007@yahoo.com>
2022-10-15 16:52:21 +0100

    commit

---
+++ sec_deploy.sh
@@ -0,0 +1,4 @@
+kubectl apply -f configurations/feed-deploy.yaml
+kubectl apply -f configurations/user-deploy.yaml
+kubectl apply -f configurations/reverseproxy-deploy.yaml
+kubectl apply -f configurations/frontend-deploy.yaml
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram microservice',
-  apiHost: 'http://a2724492ed72d4bd28b10e980561d941-1615706281.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://ad836d2d36e1341a887e7b8d7a0fd5af-939221868.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
